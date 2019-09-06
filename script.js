22c516c5b9c6071cb9e424720e86f288d8d10cbe
Jociel Barbosa <43018079+jocieltb@users.noreply.github.com>
2019-09-05 22:52:37 -0300

    Correções no layout

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
             cod = urlvideo.substring(17,68)
             document.getElementById('ifVideo').src = "https://www.youtube.com/embed/" + cod
             document.getElementById('ifVideo').hidden = false
-            
+           
         
         } else{
             cod = urlvideo.substring(17,28)
             document.getElementById('ifVideo').src = "https://www.youtube.com/embed/" + cod
             document.getElementById('ifVideo').hidden = false
         }
-        
+        document.getElementById('mostrar').hidden = false
         ocutarControles()
         tamanhoTela()
                 
@@ -24,22 +24,24 @@
       
         document.getElementById('dvcontrole').hidden = true
         document.getElementById('imgLogo').hidden = true
+        document.getElementById('mostrar').hidden = false
     }
     function mostrarControles(){
         document.getElementById('dvcontrole').hidden = false
+        document.getElementById('mostrar').hidden = true
     }
 
     function tamanhoTela(){
       var screen = window.innerWidth / window.innerHeight
 
        if(screen < 1.77){
-            document.getElementById('ifVideo').width = window.innerWidth
+            document.getElementById('ifVideo').width = window.innerWidth - 32
             document.getElementById('ifVideo').height = (document.getElementById('ifVideo').width / 16) * 9
-            alert(`screen: < ${screen}`)
+           // alert(`screen: < ${screen}`)
         } else {
-            document.getElementById('ifVideo').height = window.innerHeight
+            document.getElementById('ifVideo').height = window.innerHeight - 18
             document.getElementById('ifVideo').width = (document.getElementById('ifVideo').height / 9) * 16 
-            alert(`screen: > ${screen}`)   
+           // alert(`screen: > ${screen}`)   
         }
           
     }
